858c4b5b841aeb4c9cf54d6524198989d506aa80
kirillKozitskiy <kkozickij35@gmail.com>
2025-08-09 22:48:52 +0300

    Initial commit

---
+++ src/main/java/com/kozitskiy/dorm4/sys/Dorm4ReqSysApiApplication.java
@@ -0,0 +1,13 @@
+package com.kozitskiy.dorm4.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dorm4ReqSysApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dorm4ReqSysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kozitskiy/dorm4/sys/Dorm4ReqSysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozitskiy.dorm4.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dorm4ReqSysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
