40394b356704c31417539765e5fe2fbeb5d3ae95
kirillKozitskiy <kkozickij35@gmail.com>
2025-08-11 21:18:11 +0300

    Add GlobalExceptionHandler

---
+++ src/main/java/com/kozitskiy/dorm4/sys/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.kozitskiy.dorm4.sys.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ProblemDetail handleUserNotFoundException(UserNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(RequestNotFoundException.class)
+    public ProblemDetail handleRequestNotFoundException(RequestNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+}
